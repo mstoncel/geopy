45c18be7d1710d3f33b452014fcebe0f10b08592
Kostya Esmukov <kostya@esmukov.ru>
2018-11-17 19:14:15 +0300

    Drop Python 2.7 and 3.4 support

--- setup.py
+++ setup.py
@@ -3,16 +3,23 @@
 geopy
 """
 
+import sys
+
 from setuptools import find_packages, setup
 
 from geopy import __version__ as version
+
+if sys.version_info < (3, 5):
+    raise RuntimeError(
+        "geopy 2+ supports Python 3.5 and above. "
+        "Use geopy 1.x if you need Python 2.7 or 3.4 support."
+    )
 
 INSTALL_REQUIRES = [
     'geographiclib<2,>=1.49',
 ]
 
 EXTRAS_DEV_TESTFILES_COMMON = [
-    "contextlib2; python_version<'3.0'",
     "mock",
     "six",
 ]
@@ -25,7 +32,6 @@ EXTRAS_DEV_LINT = [
 EXTRAS_DEV_TEST = [
     "coverage",
     "pytest>=3.10",
-    "statistics; python_version<'3.0'",
 ]
 
 EXTRAS_DEV_DOCS = [
@@ -61,6 +67,7 @@ setup(
     },
     license='MIT',
     keywords='geocode geocoding gis geographical maps earth distance',
+    python_requires=">=3.5",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -70,10 +77,8 @@ setup(
         "Programming Language :: Python",
         "Topic :: Scientific/Engineering :: GIS",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
